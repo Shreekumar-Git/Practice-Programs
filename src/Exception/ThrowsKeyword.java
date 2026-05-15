6c38a6d82b598b3c583609927a61352d6230aa10
Shree Kumar <shree181203@gmail.com>
2026-05-15 16:34:11 +0530

    updated

--- src/Exception/ThrowsKeyword.java
+++ src/Exception/ThrowsKeyword.java
@@ -1,4 +1,29 @@
 package Exception;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ThrowsKeyword {
+
+    static void readfile(String filename) throws IOException{
+        try(FileReader file = new FileReader(filename)){
+            int data;
+            while((data=file.read()) != -1){
+                System.out.print((char)data);
+            }
+        }
+
+    }
+
+    public static void main(String [] args){
+        try{
+            readfile("test.txt");
+
+        }
+        catch(IOException e){
+            System.out.println("Error reading file or file not found"+ e.getMessage());
+        }
+
+        System.out.println("\n the program continues after file operation");
+    }
 }
