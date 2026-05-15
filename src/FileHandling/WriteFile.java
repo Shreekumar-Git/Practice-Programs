7ac1e9fc4220240d2d42edd1e80fd2204eed01c2
Shree Kumar <shree181203@gmail.com>
2026-05-15 08:56:31 +0530

    Update

---
+++ src/FileHandling/WriteFile.java
@@ -0,0 +1,24 @@
+package FileHandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+
+    public static void main(String []args){
+
+        try {
+            FileWriter Writer = new FileWriter("myfile.txt");
+
+            Writer.write("I want to go on a bike ride soon.");
+            Writer.close();
+
+            System.out.println("File has been written successfully");
+        }
+
+        catch(IOException e){
+            System.out.println("An error occured");
+            e.printStackTrace();
+        }
+    }
+}
