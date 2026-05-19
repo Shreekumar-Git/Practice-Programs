b3f33ba4795f8d7e1fb8ce36cedaeb246751ded6
Shree Kumar <shree181203@gmail.com>
2026-05-19 12:53:10 +0530

    updated

--- src/Array/ArrayOfObjects.java
+++ src/Array/ArrayOfObjects.java
@@ -17,14 +17,14 @@ public class ArrayOfObjects {
     public static void main(String []args){
         student [] arr;
 
-        arr = new student[5];
+        arr = new student[6];
 
         arr[0] = new student(1,"Shree");
         arr[1] = new student(2,"Anu");
         arr[2] = new student(3,"Leo");
         arr[3] = new student(4,"Baby Kannan");
         arr[4] = new student(5,"karuppu");
-//  Array index out of bound      arr[5] = new student(6,"durai singam");
+        arr[5] = new student(6,"Durai singam");
 
         for(int i = 0 ; i < arr.length ; i++ ){
             System.out.println("Element at "+ i +": { "+ arr[i].roll_no+ " , " + arr[i].name + " }");
